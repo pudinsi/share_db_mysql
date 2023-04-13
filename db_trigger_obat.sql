fcfe3063d10da19e8a419c48dce21d5810fc4b81
Pudin Saepudin Ilham <128332783+pudinsi@users.noreply.github.com>
2023-04-13 13:10:20 +0700

    Update db_trigger_obat.sql

--- db_trigger_obat.sql
+++ db_trigger_obat.sql
@@ -8,7 +8,10 @@ CREATE TABLE IF NOT EXISTS `obat` (
   `id_obat` int(11) NOT NULL AUTO_INCREMENT,
   `obat_kode` varchar(100) NOT NULL,
   `obat_nama` varchar(150) NOT NULL,
+  `obat_satuan` varchar(20) NOT NULL,
   `obat_stok` int(11) NOT NULL,
+  `obat_create` datetime NOT NULL,
+  `obat_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_obat`),
   CONSTRAINT `uc_obat_kode` UNIQUE (`obat_kode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -19,6 +22,8 @@ CREATE TABLE IF NOT EXISTS `obatMasuk` (
   `om_obatkode` varchar(100) NOT NULL,
   `om_tanggal` date NOT NULL,
   `om_jumlah` int(11) NOT NULL,
+  `om_create` datetime NOT NULL,
+  `om_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_om`),
   CONSTRAINT `fk_obat_obatmasuk1` FOREIGN KEY (`om_obatkode`) REFERENCES `obat` (`obat_kode`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -28,6 +33,8 @@ CREATE TABLE IF NOT EXISTS `obatKeluar` (
   `ok_obatkode` varchar(100) NOT NULL,
   `ok_tanggal` date NOT NULL,
   `ok_jumlah` int(11) NOT NULL,
+  `ok_create` datetime NOT NULL,
+  `ok_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_ok`),
   CONSTRAINT `fk_obat_obatkeluar1` FOREIGN KEY (`ok_obatkode`) REFERENCES `obat` (`obat_kode`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -177,18 +184,19 @@ GROUP BY ok_obatkode;
 --
 -- /* ------------------------------------------------------------------------- */
 --
--- VIEW OBAT berikut obat masuk, obat keluar dan stok tersedianya.
+-- VIEW OBAT JOIN berikut obat masuk, obat keluar dan stok tersedianya.
 --
 
 CREATE VIEW v_obat
 AS
 SELECT 
-tb1.id_obat, tb1.obat_kode, tb1.obat_nama, tb1.obat_stok,
+tb1.id_obat, tb1.obat_kode, tb1.obat_nama, tb1.obat_stok, tb1.obat_satuan,
 tb2.om_jml,
 tb3.ok_jml
 FROM obat tb1
 LEFT JOIN v_obatmasuk tb2 ON tb1.obat_kode = tb2.om_obatkode
 LEFT JOIN v_obatkeluar tb3 ON tb1.obat_kode = tb3.ok_obatkode;
+
 
 --
 -- VIEW JOIN
@@ -198,7 +206,7 @@ CREATE VIEW v_j_obatmasuk
 AS
 SELECT 
 tb1.id_om, om_obatkode, tb1.om_tanggal, tb1.om_jumlah,
-tb2.obat_nama
+tb2.obat_nama, tb2.obat_satuan
 FROM obatmasuk tb1
 LEFT JOIN obat tb2 ON tb1.om_obatkode = tb2.obat_kode;
 
@@ -206,7 +214,7 @@ CREATE VIEW v_j_obatkeluar
 AS
 SELECT 
 tb1.id_ok, ok_obatkode, tb1.ok_tanggal, tb1.ok_jumlah,
-tb2.obat_nama
+tb2.obat_nama, tb2.obat_satuan
 FROM obatkeluar tb1
 LEFT JOIN obat tb2 ON tb1.ok_obatkode = tb2.obat_kode;
 
